8e5832089b2ea33049326ad88c9368de9e28cca1
Erew42 <erik96@gmx.net>
2026-02-13 16:39:16 +0100

    Update sec_ccm_contracts.py

--- src/thesis_pkg/core/ccm/sec_ccm_contracts.py
+++ src/thesis_pkg/core/ccm/sec_ccm_contracts.py
@@ -40,7 +40,7 @@ class SecCcmJoinSpecV1:
         "SHRCD",
         "EXCHCD",
         "VOL",
-        "MKT_CAP",
+        # "MKT_CAP",
         "TCAP",
     )
     required_daily_non_null_features: tuple[str, ...] = ()
